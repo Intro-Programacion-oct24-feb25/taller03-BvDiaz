bf8df9079647f890604bcdcf8f7dfc23c59be3be
Bryan Diaz <vicentepomas.2007@gmail.com>
2024-10-24 12:12:59 -0500

    CAMBIOS2

--- Ejercicio10_/src/ejercicio10_/Ejercicio10_.java
+++ Ejercicio10_/src/ejercicio10_/Ejercicio10_.java
@@ -2,22 +2,23 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
  */
-package ejercicio09;
+package ejercicio10_;
 
 /**
  *
  * @author utpl
  */
-public class Ejercicio09 {
+public class Ejercicio10_ {
 
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        double a = Math.sqrt(81);
-        double b = Math.sqrt(25) * 10;
-        double resultado = a + b;
-        System.out.println(resultado >= 50);
+        boolean a = Math.sqrt(25) * 10 >= 100;
+        boolean b = true;
+        boolean c = false;
+        boolean d = (10/5) >= 2;
+        System.out.println(a && b || c || d);
     }
     
 }
